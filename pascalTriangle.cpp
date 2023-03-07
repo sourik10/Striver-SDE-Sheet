c046a0bc8cb2e6f5d11c5f9c01d07ad8cf6ad119
Sourik <73734325+sourik10@users.noreply.github.com>
2023-03-07 12:16:21 +0530

    Create pascalTriangle.cpp

---
+++ pascalTriangle.cpp
@@ -0,0 +1,14 @@
+// https://www.youtube.com/watch?v=6FLvhQjZqvM&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=9
+#include <bits/stdc++.h> 
+vector<vector<long long int>> printPascal(int n){
+  vector<vector<long long int>> r(n);
+  for(int i=0;i<n;i++){
+      r[i].resize(i+1);
+      r[i][0]=r[i][i]=1;
+
+      for (int j = 1; j < i; j++) {
+        r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+      }
+  }
+  return r;
+}
